2fe1e07a1d2128eaffbe6f1298f6f11cdff79058
Hamza Faiazov <anderforz80@gmail.com>
2022-06-20 18:29:10 +0300

    modifided_2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const number_group = document.querySelectorAll('.number'),
   procent_button = document.querySelector('.procent'),
   input = document.querySelector('.input')
 
-// dictionary of signs
+// dictionary of signs //словарь знаков
 const dict_signs = {
   '÷': '/',
   '×': '*',
@@ -15,35 +15,40 @@ const dict_signs = {
   '=': '=',
 };
 
-// array for calculation
+// array for calculation //массив для расчета
 let arr_numbers = [],
-  // string for numbers
-  str_number = '',
-  // string for save first number after calculation
-  save_number = '',
-  // signs (operator of calculation)
-  sign = '',
-  // save sign for iteration click equal
-  save_sign = '',
-  // state of signs clickable
+  // string for numbers //строка для чисел
+  str_number = "",
+  // string for save first number after calculation //строка для сохранения первого числа после вычисления
+  save_number = "",
+  // signs (operator of calculation) // признаки (оператор вычисления)
+  sign = "",
+  // save sign for iteration click equal //сохранить знак для итерации нажать равно
+  save_sign = "",
+  // state of signs clickable //состояние знаков кликабельно
   sign_state = true,
-  // state of equal clicked
-  equal_state = false;
+  // state of equal clicked //состояние равного щелчка
+  equal_state = false,
 
-// add numbers and save
+  new_exeption_state = false;
+
+  
+
+// add numbers and save //добавить номера и сохранить
 function addNumberToArray() {
+  // clearInputIfPressAnyButton();
   let number = this.textContent;
   str_number += number;
   input.value = str_number;
   save_number = str_number;
   sign_state = true;
 }
-// add sign and push to array
+// add sign and push to array //добавить знак и нажать на массив
 function addSignToArray(sign) {
   save_sign = sign;
   if (sign_state) {
-    str_number != '' ? arr_numbers.push(str_number) : null;
-    str_number = '';
+    str_number != "" ? arr_numbers.push(str_number) : null;
+    str_number = "";
     arr_numbers.push(sign);
     equal_state = false;
     sign_state = false;
@@ -51,24 +56,32 @@ function addSignToArray(sign) {
   input.value = '';
 }
 
-// events of buttons
+// events of buttons //события кнопок
 number_group.forEach((item) => {
   item.addEventListener('click', addNumberToArray)
 })
 
-// sign equal
+// sign equal //знак равенства
 function equal(len, sign) {
+  // console.log('1 ',str_number)
   len < 1 ? arr_numbers = ['0'] : null;
+  // console.log("2 ", str_number);
   arr_numbers[len-1] == sign ? arr_numbers.push('0') : null;
+  // console.log("3 ", str_number);
   str_number != '' ? arr_numbers.push(str_number) : null;
-  str_number = eval(arr_numbers.join(''));
+  // console.log("4 ", str_number);
+  console.log('arr numbers = ',arr_numbers);
+  // str_number = eval(arr_numbers.join(''));
+  str_number = parseFloat(arr_numbers.join(''));
+  // console.log("5 ", str_number);
+  console.log('str_number = ',str_number,' sign = ',sign,' save number = ',save_number)
   equal_state ? str_number = eval(str_number + sign + save_number) : null;
   input.value = str_number;
   arr_numbers = [];
   arr_numbers[0] = str_number;
-  equal_state = true;
+  equal_state = true; 
 }
-// calculation
+// calculation //расчет
 function calc() {
   sign = this.textContent;
   const numbers_len = arr_numbers.length;
@@ -100,15 +113,14 @@ function calc() {
       break;
   }
   str_number = '';
-  console.log(arr_numbers)
 }
 
-// events of signs
+// events of signs //события знаков
 sign_group.forEach((item) => {
   item.addEventListener('click', calc);
 });
 
-// clear field and variables
+// clear field and variables //очистить поле и переменные
 function clear() {
   input.value = '',
   arr_numbers = [''],
@@ -118,5 +130,24 @@ function clear() {
   sign = ''
 }
 
-// clear event
+// clear event //очистить событие
 clear_button.addEventListener('click', clear);
+
+// clear input after pressing any button, if button is pressed equals 
+// очистить инпут после нажатия любой кнопки, если нажата кнопка равно 
+function clearInputIfPressAnyButton () {
+  if (equal_state == true) {
+      clear()
+      equal_state = false;
+    }
+  }
+//
+procent_button.addEventListener('click',procentCalc)
+
+function procentCalc () {
+  console.log(save_number)
+  save_number = save_number / 100 
+  input.value = save_number
+  arr_numbers.push(save_number);
+  sign_state = true;
+}
